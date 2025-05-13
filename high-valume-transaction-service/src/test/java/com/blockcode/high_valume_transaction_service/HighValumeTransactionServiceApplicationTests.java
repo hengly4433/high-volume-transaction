55f0c1f54f35391ed4e1fd7312837b92775c7087
Hengly Siev <hengly.s@pipay.com>
2025-05-13 18:05:07 +0700

    Create initial project structure

---
+++ high-valume-transaction-service/src/test/java/com/blockcode/high_valume_transaction_service/HighValumeTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockcode.high_valume_transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighValumeTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
